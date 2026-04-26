d0c1bfed7aad0e0b36be3694e306799fefd51b01
Hrvoje Rubesa <hrvoje.rubesa@icloud.com>
2026-04-26 19:39:34 +0000

    implemented time dimension model

--- models/group8_project/marts/shared/dim_time.sql
+++ models/group8_project/marts/shared/dim_time.sql
@@ -5,7 +5,7 @@ WITH all_times AS (
 
     -- Time component from 311 created_date (timestamp → time)
     SELECT DISTINCT
-        EXTRACT(HOUR   FROM created_date) AS hour,
+        EXTRACT(HOUR FROM created_date) AS hour,
         EXTRACT(MINUTE FROM created_date) AS minute
     FROM {{ ref('stg_nyc_311_dot') }}
     WHERE created_date IS NOT NULL
@@ -14,10 +14,28 @@ WITH all_times AS (
 
     -- Time from collisions crash_time
     SELECT DISTINCT
-        EXTRACT(HOUR   FROM crash_time) AS hour,
+        EXTRACT(HOUR FROM crash_time) AS hour,
         EXTRACT(MINUTE FROM crash_time) AS minute
     FROM {{ ref('stg_motor_vehicle_collisions') }}
     WHERE crash_time IS NOT NULL
 
 ),
 
+time_dimension AS (
+
+    SELECT
+        {{ dbt_utils.generate_surrogate_key(['hour', 'minute']) }} AS time_key,
+        hour,
+        minute,
+        CASE
+            WHEN hour BETWEEN 5  AND 11 THEN 'Morning'
+            WHEN hour BETWEEN 12 AND 16 THEN 'Afternoon'
+            WHEN hour BETWEEN 17 AND 20 THEN 'Evening'
+            ELSE                             'Night'
+        END AS time_period
+    FROM all_times
+
+)
+
+SELECT *
+FROM time_dimension
