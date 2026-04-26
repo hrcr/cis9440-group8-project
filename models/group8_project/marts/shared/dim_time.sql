d98dba0517f9ce44bf484f0f2fb9c819665cbc27
Hrvoje Rubesa <hrvoje.rubesa@icloud.com>
2026-04-26 22:03:42 +0000

    syntax correction for dim_time

--- models/group8_project/marts/shared/dim_time.sql
+++ models/group8_project/marts/shared/dim_time.sql
@@ -3,23 +3,25 @@
 
 WITH all_times AS (
 
-    -- Time component from 311 created_date (timestamp → time)
-    SELECT DISTINCT
-        EXTRACT(HOUR FROM created_date) AS hour,
-        EXTRACT(MINUTE FROM created_date) AS minute
-    FROM {{ ref('stg_nyc_311_dot') }}
-    WHERE created_date IS NOT NULL
+    (
+        -- Time component from 311 created_date (timestamp → time)
+        SELECT DISTINCT
+            EXTRACT(HOUR   FROM created_date) AS hour,
+            EXTRACT(MINUTE FROM created_date) AS minute
+        FROM {{ ref('stg_nyc_311_dot') }}
 
-    UNION DISTINCT
+        UNION DISTINCT
 
-    -- Time from collisions crash_time
-    SELECT DISTINCT
-        EXTRACT(HOUR FROM crash_time) AS hour,
-        EXTRACT(MINUTE FROM crash_time) AS minute
-    FROM {{ ref('stg_motor_vehicle_collisions') }}
-    WHERE crash_time IS NOT NULL
+        -- Time from collisions crash_time
+        SELECT DISTINCT
+            EXTRACT(HOUR   FROM crash_time) AS hour,
+            EXTRACT(MINUTE FROM crash_time) AS minute
+        FROM {{ ref('stg_motor_vehicle_collisions') }}
+    )
+
     UNION ALL
 
+    -- Sentinel NULL row for any nullable time FK in fact tables.
     SELECT NULL AS hour, NULL AS minute
 
 ),
