67657280bacbba33c78391094d65cf0ee155542b
Hrvoje Rubesa <hrvoje.rubesa@icloud.com>
2026-05-02 21:13:18 +0000

    removed raws from fact_collission

--- models/group8_project/marts/fact_tables/fact_collision.sql
+++ models/group8_project/marts/fact_tables/fact_collision.sql
@@ -81,12 +81,6 @@ joined AS (
         COALESCE(stg.number_of_persons_injured, 0) +
         COALESCE(stg.number_of_persons_killed,  0) AS total_victims,
 
-        -- Degenerate dimensions (street names kept on fact, not worth own dim)
-        stg.on_street_name,
-        stg.off_street_name,
-        stg.cross_street_name,
-        stg.latitude,
-        stg.longitude,
 
 
     FROM stg
