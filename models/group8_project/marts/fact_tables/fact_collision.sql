f0e93a7a52d6146d8e647144dfddc70fdffe2a53
Hrvoje Rubesa <hrvoje.rubesa@icloud.com>
2026-05-02 18:18:22 +0000

    reordered columns in fact collision

--- models/group8_project/marts/fact_tables/fact_collision.sql
+++ models/group8_project/marts/fact_tables/fact_collision.sql
@@ -104,13 +104,6 @@ joined AS (
                               AND stg.borough         = dl.borough
                               AND stg.zip_code        = dl.zip_code
 
-    -- Contributing factor slots
-    LEFT JOIN dim_cf cf1 ON stg.contributing_factor_vehicle_1 = cf1.contributing_factor_for_vehicle
-    LEFT JOIN dim_cf cf2 ON stg.contributing_factor_vehicle_2 = cf2.contributing_factor_for_vehicle
-    LEFT JOIN dim_cf cf3 ON stg.contributing_factor_vehicle_3 = cf3.contributing_factor_for_vehicle
-    LEFT JOIN dim_cf cf4 ON stg.contributing_factor_vehicle_4 = cf4.contributing_factor_for_vehicle
-    LEFT JOIN dim_cf cf5 ON stg.contributing_factor_vehicle_5 = cf5.contributing_factor_for_vehicle
-
     -- Vehicle type slots
     LEFT JOIN dim_veh vt1 ON stg.vehicle_type_1 = vt1.vehicle_type
     LEFT JOIN dim_veh vt2 ON stg.vehicle_type_2 = vt2.vehicle_type
@@ -118,6 +111,12 @@ joined AS (
     LEFT JOIN dim_veh vt4 ON stg.vehicle_type_4 = vt4.vehicle_type
     LEFT JOIN dim_veh vt5 ON stg.vehicle_type_5 = vt5.vehicle_type
 
+    -- Contributing factor slots
+    LEFT JOIN dim_cf cf1 ON stg.contributing_factor_vehicle_1 = cf1.contributing_factor_for_vehicle
+    LEFT JOIN dim_cf cf2 ON stg.contributing_factor_vehicle_2 = cf2.contributing_factor_for_vehicle
+    LEFT JOIN dim_cf cf3 ON stg.contributing_factor_vehicle_3 = cf3.contributing_factor_for_vehicle
+    LEFT JOIN dim_cf cf4 ON stg.contributing_factor_vehicle_4 = cf4.contributing_factor_for_vehicle
+    LEFT JOIN dim_cf cf5 ON stg.contributing_factor_vehicle_5 = cf5.contributing_factor_for_vehicle
 )
 
 SELECT
